ec733b493089b3b8c0585afa5b587a4a3ed219f4
Snehdeep Singh <singhsnehdeep99@gmail.com>
2024-12-29 15:58:30 +0530

    github authentication

---
+++ src/app/api/callback.js
@@ -0,0 +1,33 @@
+import { AuthorizationCode } from 'simple-oauth2';
+
+export default async function handler(req, res) {
+  const host = req.headers.host;
+  const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+  const baseUrl = `${protocol}://${host}`;
+
+  const config = {
+    client: {
+      id: process.env.OAUTH_CLIENT_ID,
+      secret: process.env.OAUTH_CLIENT_SECRET,
+    },
+    auth: {
+      tokenHost: 'https://github.com',
+      tokenPath: '/login/oauth/access_token',
+      authorizePath: '/login/oauth/authorize',
+    },
+  };
+
+  const { code } = req.query;
+  const client = new AuthorizationCode(config);
+
+  try {
+    const accessToken = await client.getToken({
+      code,
+      redirect_uri: `${baseUrl}/api/callback`,
+    });
+
+    res.redirect('/admin/#access_token=' + accessToken.token.access_token);
+  } catch (error) {
+    res.status(500).json('Authentication failed');
+  }
+}
